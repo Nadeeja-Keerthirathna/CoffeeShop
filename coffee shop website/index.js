085bac453cbf92ed164b3cb5d265cfcfffd489ac
nadeeja0 <nadeejakeerthirathna605@gmail.com>
2025-09-11 18:13:57 +0530

    reviews

--- coffee shop website/index.js
+++ coffee shop website/index.js
@@ -9,3 +9,35 @@ menuopenbutton.addEventListener("click", ()=>{
 menuclosebutton.addEventListener("click",()=>{
     document.body.classList.remove("show-mobile-menu");
 })
+
+const swiper = new Swiper('.slider-wrapper', {
+  grabCursor:true,
+  loop: true,
+  spaceBetween:25, 
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable:true,
+    dynamicBullets:true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints:{
+    0:{
+        slidesPerView:1
+    },
+     768:{
+        slidesPerView:2
+    },
+     1024:{
+        slidesPerView:3
+    }
+  }
+
+});
